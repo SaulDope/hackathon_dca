1654be2f05b9404bbacbaf8d9d4d40c84d7482c4
bubby.eth <112112385+bubby-eth@users.noreply.github.com>
2023-10-15 17:11:08 -0700

    Styled tables

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,13 @@ import { Connect } from "../components/Wallet";
 
 export function Header() {
   return (
-    <div>
-      <div className="header">
-        <div className="logo-section">
-          <img src="/logo-white.png" className="logo" />
-          <span>DCA ONCHAIN</span>
-        </div>
-        <div className="wallet-section">
-          <Connect />
-        </div>
+    <div className="header">
+      <div className="logo-section">
+        <img src="/logo-white.png" className="logo" />
+        <span>DCA ONCHAIN</span>
+      </div>
+      <div className="wallet-section">
+        <Connect />
       </div>
     </div>
   );
--- src/components/Tables.tsx
+++ src/components/Tables.tsx
@@ -1,65 +1,73 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
 
-const Tables = () => {
-  const [activeTab, setActiveTab] = useState("strategies");
-  const [isModalOpen, setModalOpen] = useState(false);
+type Strategy = {
+  name: string;
+  pair: string;
+  dex: string;
+  buysPerDay: string;
+  avgFeesPerBuy: string;
+  actions: string;
+};
 
-  const modalRef = useRef<HTMLDivElement | null>(null); // Reference for the modal
+const strategiesData: Strategy[] = [
+  {
+    name: "WBTC",
+    pair: "WBTC-USDC",
+    dex: "UNISWAP",
+    buysPerDay: "4",
+    avgFeesPerBuy: "0.01 ETH",
+    actions: "DEPOSIT",
+  },
+  {
+    name: "ETH",
+    pair: "ETH-USDC",
+    dex: "UNISWAP",
+    buysPerDay: "3",
+    avgFeesPerBuy: "0.005 ETH",
+    actions: "DEPOSIT",
+  },
+  {
+    name: "LINK",
+    pair: "LINK-USDC",
+    dex: "UNISWAP",
+    buysPerDay: "2",
+    avgFeesPerBuy: "0.002 ETH",
+    actions: "DEPOSIT",
+  },
+  {
+    name: "UNI",
+    pair: "UNI-USDC",
+    dex: "UNISWAP",
+    buysPerDay: "5",
+    avgFeesPerBuy: "0.015 ETH",
+    actions: "DEPOSIT",
+  },
+];
+
+const Tables = () => {
+  const [activeTab, setActiveTab] = useState<string>("strategies");
+  const [isModalOpen, setModalOpen] = useState<boolean>(false);
+
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (
+      isModalOpen &&
+      modalRef.current &&
+      !modalRef.current.contains(event.target as Node)
+    ) {
+      setModalOpen(false);
+    }
+  };
 
   useEffect(() => {
-    function handleClickOutside(event: { target: any }) {
-      if (
-        isModalOpen &&
-        modalRef.current &&
-        !modalRef.current.contains(event.target)
-      ) {
-        setModalOpen(false);
-      }
-    }
-
     document.addEventListener("mousedown", handleClickOutside);
 
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [isModalOpen]);
-
-  //Dummy data
-  const strategiesData = [
-    {
-      name: "WBTC",
-      pair: "WBTC-USDC",
-      dex: "UNISWAP",
-      buysPerDay: "4",
-      avgFeesPerBuy: "0.01 ETH",
-      actions: "DEPOSIT",
-    },
-    {
-      name: "ETH",
-      pair: "ETH-USDC",
-      dex: "UNISWAP",
-      buysPerDay: "3",
-      avgFeesPerBuy: "0.005 ETH",
-      actions: "DEPOSIT",
-    },
-    {
-      name: "LINK",
-      pair: "LINK-USDC",
-      dex: "UNISWAP",
-      buysPerDay: "2",
-      avgFeesPerBuy: "0.002 ETH",
-      actions: "DEPOSIT",
-    },
-    {
-      name: "UNI",
-      pair: "UNI-USDC",
-      dex: "UNISWAP",
-      buysPerDay: "5",
-      avgFeesPerBuy: "0.015 ETH",
-      actions: "DEPOSIT",
-    },
-  ];
 
   return (
     <div className="tabs">
@@ -134,7 +142,7 @@ const Tables = () => {
             </button>
           </div>
           <div className="modal-content">
-            {/* Add your modal content here */}
+            {/* Add modal content here */}
             <p>Deposit modal functions</p>
           </div>
         </div>
